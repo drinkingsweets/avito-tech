d696594f0d3479ef187e8726f22d4f96e123ec84
drinking_sweets <marrk@internet.ru>
2025-11-12 16:48:03 +0300

    initial commit

---
+++ src/main/java/org/example/avitotech/AvitoTechApplication.java
@@ -0,0 +1,13 @@
+package org.example.avitotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitoTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvitoTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/avitotech/AvitoTechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.avitotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
